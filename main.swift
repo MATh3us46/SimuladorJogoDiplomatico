3485c8dd9ab96f256dab287ae42d0a68a6b14af6
MATh3us46 <matheussavegnago735@gmail.com>
2026-06-09 17:01:33 -0300

    2

--- main.swift
+++ main.swift
@@ -1,96 +1,53 @@
 //------------------------ CLASSE PAIS ------------------------
-public class Pais {
-    //Variaveis 
-    private var _nome: String
-    private var _economia: Int
-    private var _poderPolitico: Int
-    private var _tamanhoExercito: Int
+public class Pais{
+    public var nome: String
+    public var economia: Int
+    public var poderPolitico: Int
+    public var tamanhoExercito: Int
 
-    //Construtor 
     public init(nome: String, economia: Int, poderPolitico: Int, tamanhoExercito: Int){
-        self._nome = nome
-        self._economia = economia
-        self._poderPolitico = poderPolitico
-        self._tamanhoExercito = tamanhoExercito
+        self.nome = nome
+        self.economia = economia
+        self.poderPolitico = poderPolitico
+        self.tamanhoExercito = tamanhoExercito
     }
 
-    //Metodos
     public func ganhoPoderPolitico(){
-        self._poderPolitico += 10
+        self.poderPolitico += 10
         print("Poder Politico: \(poderPolitico)") 
     }
 
     public func treinamentoMilitar(){
-        self._tamanhoExercito += 500
+        self.tamanhoExercito += 500
         print("Tamanho do exercito: \(tamanhoExercito)") 
     }
 
-    //Regra de Negocio 1°
     public func treinamentoMilitar(pPolitico: Int){
-        if self._poderPolitico >= pPolitico{
-            self._poderPolitico -= pPolitico;
-            self._tamanhoExercito += (pPolitico * 2)
+        if self.poderPolitico >= pPolitico{
+            self.poderPolitico -= pPolitico
+            self.tamanhoExercito += (pPolitico * 2)
             print("Tamanho do exercito: \(tamanhoExercito)")
-        }
+        } 
         else{
             print("Nao possui Poder Politico Suficiente")
-        }
-    }
-
-    //Getters e Setters
-    public var nome: String{
-        get {
-            return _nome
-        }
-        set {
-            _nome = newValue 
-        }
-    }
-
-    public var economia: Int{
-        get {
-            return _economia
-        }
-        set {
-            _economia = newValue 
-        }
-    }
-
-    public var poderPolitico: Int{
-        get {
-            return _poderPolitico
-        }
-        set {
-            _poderPolitico = newValue 
-        }
-    }
-
-    public var tamanhoExercito: Int{
-        get {
-            return _tamanhoExercito
-        }
-        set {
-            _tamanhoExercito = newValue 
         }
     }
 }
 
 //------------------------ CLASSE DEMOCRACIA ------------------------
 public class Democracia: Pais{
-    private var _suportePopular: Bool
-    //Construtor 
+    public var suportePopular: Bool
+    
     public init(nome: String, economia: Int, poderPolitico: Int, tamanhoExercito: Int, suportePopular: Bool){
-        self._suportePopular = suportePopular
+        self.suportePopular = suportePopular
         super.init(nome: nome, economia: economia, poderPolitico: poderPolitico, tamanhoExercito: tamanhoExercito)
     }
 
-    //Metodos
     override public func ganhoPoderPolitico(){
-        if self._suportePopular{
+        if self.suportePopular {
             self.poderPolitico += 15
             print("+15 Poder Politico")
-        } 
-        else{
+        } else {
             self.poderPolitico += 5
             print("+5 Poder Politico")
         }
@@ -103,32 +60,19 @@ public class Democracia: Pais{
         print("Tamanho do Exercito: \(tamanhoExercito)")
         print("Suporte Popular: \(suportePopular ? "Possui" : "Nao Possui")")
     }
-
-    //Getters e Setters
-    public var suportePopular: Bool{
-        get {
-            return _suportePopular
-        }
-        set {
-            _suportePopular = newValue 
-        }
-    }
 }
 
 //------------------------ CLASSE DITADURA ------------------------
 public class Ditadura: Pais{
-    //Variaveis 
-    private var _militarizacao: Bool
+    public var militarizacao: Bool
 
-    //Construtor 
     public init(nome: String, economia: Int, poderPolitico: Int, tamanhoExercito: Int, militarizacao: Bool){
-        self._militarizacao = militarizacao
+        self.militarizacao = militarizacao
         super.init(nome: nome, economia: economia, poderPolitico: poderPolitico, tamanhoExercito: tamanhoExercito)
     }
 
-    //Metodos
     override public func treinamentoMilitar(){
-        if self._militarizacao{
+        if self.militarizacao{
             self.tamanhoExercito += 1000
             print("+1000 Soldados")
         } 
@@ -138,20 +82,91 @@ public class Ditadura: Pais{
         }
     }
 
-    //Getters e Setters
-    public var militarizacao: Bool{
-        get {
-            return _militarizacao
+    public func exibirInformacoes(){
+        print("Nome: \(nome)") 
+        print("Economia: \(economia)") 
+        print("Poder Politico: \(poderPolitico)") 
+        print("Tamanho do Exercito: \(tamanhoExercito)")
+        print("Militarizacao: \(militarizacao ? "Possui" : "Nao Possui")")
+    }
+}
+
+//------------------------ CLASSE FACCOES (AGREGAÇÃO) ------------------------
+public class Faccao {
+    public var nomeFaccao: String
+    public var lider: Pais
+    public var membros: [Pais]
+
+    public init(nomeFaccao: String, lider: Pais){
+        self.nomeFaccao = nomeFaccao
+        self.lider = lider
+        self.membros = [Pais]()
+        self.membros.append(lider)
+    }
+
+    public func adicionarMembro(novoMembro: Pais){
+        self.membros.append(novoMembro)
+        print("\(novoMembro.nome) juntou-se a facao \(nomeFaccao)")
+    }
+
+    public func exibirFaccao(){
+        print("Nome: \(nomeFaccao)")
+        print("Lider: \(lider.nome)") 
+        print("Membros: ")
+        for i in self.membros {
+            print(" - \(i.nome)")
         }
-        set {
-            _militarizacao = newValue 
+    }
+}
+
+//------------------------ CLASSE AÇÃO DIPLOMÁTICA (ASSOCIAÇÃO) ------------------------
+public class AcaoDiplomatica {
+    public var tipo: String
+    public var remetente: Pais
+    public var destinatario: Pais
+    public var aceito: Bool
+
+    public init(tipo: String, remetente: Pais, destinatario: Pais, aceito: Bool){
+        self.tipo = tipo
+        self.remetente = remetente
+        self.destinatario = destinatario
+        self.aceito = aceito
+    }
+
+    public func enviarProposta(){
+        let custoPP: Int = 15
+        print("O pais \(remetente.nome) propos um \(tipo) para \(destinatario.nome)")
+
+        if remetente.poderPolitico < custoPP{
+            print("Acao cancelada: \(remetente.nome) nao possui Poder Politico suficiente para enviar a proposta (minimo: \(custoPP))")
+            self.aceito = false
+            return
+        }
+
+        remetente.poderPolitico = remetente.poderPolitico - custoPP
+        print("\(remetente.nome) gastou \(custoPP) de Poder Politico")
+
+        if destinatario is Ditadura{
+            if remetente.poderPolitico >= destinatario.poderPolitico / 2{
+                print("\(destinatario.nome) aceitou o pacto de \(remetente.nome)")
+                self.aceito = true
+            } 
+            else{
+                print("\(destinatario.nome) recusou o pacto de \(remetente.nome)")
+                self.aceito = false
+            }
+        } 
+        else if let demo = destinatario as? Democracia {
+            if demo.suportePopular {
+                print("\(destinatario.nome) aceitou o pacto de \(remetente.nome)")
+                self.aceito = true
+            } 
+            else {
+                print("\(destinatario.nome) recusou o pacto de \(remetente.nome)")
+                self.aceito = false
+            }
         }
     }
 }
 
 //------------------------ CLASSE MAIN ------------------------
-let Franca = Ditadura(nome: "Franca", economia: 200, poderPolitico: 70, tamanhoExercito: 100000, militarizacao: true)
-
-Franca.treinamentoMilitar(pPolitico: 15)
-Franca.treinamentoMilitar();
-
