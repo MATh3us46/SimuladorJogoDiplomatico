a2054eca3ba4be78b577ca78fd72e2def925e764
Matheus Savegnago <126601932+MATh3us46@users.noreply.github.com>
2026-06-11 21:15:45 -0300

    Refactor Ditadura class and add militarization method
    Refactor Ditadura class to use private(set) for militarizacao and add alterarMilitarizacao method.

--- Main/Ditadura.swift
+++ Main/Ditadura.swift
@@ -1,27 +1,31 @@
-public class Ditadura: Pais{
-    public var militarizacao: Bool
+public class Ditadura: Pais {
+    public private(set) var militarizacao: Bool
 
-    public init(nome: String, economia: Int, poderPolitico: Int, tamanhoExercito: Int, militarizacao: Bool){
+    public init(nome: String, economia: Int, poderPolitico: Int, tamanhoExercito: Int, militarizacao: Bool) {
         self.militarizacao = militarizacao
         super.init(nome: nome, economia: economia, poderPolitico: poderPolitico, tamanhoExercito: tamanhoExercito)
     }
 
-    override public func treinamentoMilitar(){
-        if self.militarizacao{
-            self.tamanhoExercito += 1000
+    override public func treinamentoMilitar() {
+        if self.militarizacao {
+            self.alterarTamanhoExercito(valor: 1000)
             print("+1000 Soldados")
         } 
-        else{
-            self.tamanhoExercito += 750
+        else {
+            self.alterarTamanhoExercito(valor: 750)
             print("+750 Soldados")
         }
     }
 
-    public func exibirInformacoes(){
+    public func exibirInformacoes() {
         print("Nome: \(nome)") 
         print("Economia: \(economia)") 
         print("Poder Politico: \(poderPolitico)") 
         print("Tamanho do Exercito: \(tamanhoExercito)")
         print("Militarizacao: \(militarizacao ? "Possui" : "Nao Possui")")
+    }
+
+    public func alterarMilitarizacao(novoStatus: Bool) {
+        self.militarizacao = novoStatus
     }
 }
