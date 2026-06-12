4c6ae900cbd83dca3a666f67a0fb0aa92dcb5171
Matheus Savegnago <126601932+MATh3us46@users.noreply.github.com>
2026-06-11 21:15:26 -0300

    Refactor Democracia class and add support method
    Refactor Democracia class to use private(set) for suportePopular and add alterarSuportePopular method.

--- Main/Democracia.swift
+++ Main/Democracia.swift
@@ -1,26 +1,30 @@
-public class Democracia: Pais{
-    public var suportePopular: Bool
+public class Democracia: Pais {
+    public private(set) var suportePopular: Bool
     
-    public init(nome: String, economia: Int, poderPolitico: Int, tamanhoExercito: Int, suportePopular: Bool){
+    public init(nome: String, economia: Int, poderPolitico: Int, tamanhoExercito: Int, suportePopular: Bool) {
         self.suportePopular = suportePopular
         super.init(nome: nome, economia: economia, poderPolitico: poderPolitico, tamanhoExercito: tamanhoExercito)
     }
 
-    override public func ganhoPoderPolitico(){
+    override public func ganhoPoderPolitico() {
         if self.suportePopular {
-            self.poderPolitico += 15
+            self.alterarPoderPolitico(valor: 15)
             print("+15 Poder Politico")
         } else {
-            self.poderPolitico += 5
+            self.alterarPoderPolitico(valor: 5)
             print("+5 Poder Politico")
         }
     }
     
-    public func exibirInformacoes(){
+    public func exibirInformacoes() {
         print("Nome: \(nome)") 
         print("Economia: \(economia)") 
         print("Poder Politico: \(poderPolitico)") 
         print("Tamanho do Exercito: \(tamanhoExercito)")
         print("Suporte Popular: \(suportePopular ? "Possui" : "Nao Possui")")
+    }
+
+    public func alterarSuportePopular(novoStatus: Bool) {
+        self.suportePopular = novoStatus
     }
 }
